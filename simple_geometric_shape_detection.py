6304722bc7e0f854ea9a64e7169e8f4be35f8168
Tiago Valdrich <tiago.valdrich@hotmail.com>
2019-08-30 08:54:15 -0300

    Adjusting the text position on geometric shape detection

--- simple_geometric_shape_detection.py
+++ simple_geometric_shape_detection.py
@@ -14,21 +14,21 @@ for contour in contours:
     y = approx.ravel()[1]
 
     if len(approx) == 3:
-        cv2.putText(img, "Triangle", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+        cv2.putText(img, "Triangle", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
     elif len(approx) == 4:
         x, y, w, h = cv2.boundingRect(approx)
         aspect_ratio = float(w) / h        
 
         if aspect_ratio >= 0.95 and aspect_ratio <= 1.05:
-            cv2.putText(img, "Square", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+            cv2.putText(img, "Square", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
         else:
-            cv2.putText(img, "Rectangle", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+            cv2.putText(img, "Rectangle", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
     elif len(approx) == 5:
-        cv2.putText(img, "Pentagon", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+        cv2.putText(img, "Pentagon", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
     elif len(approx) == 10:
-        cv2.putText(img, "Star", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+        cv2.putText(img, "Star", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
     else:
-        cv2.putText(img, "Circle", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+        cv2.putText(img, "Circle", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
 
 cv2.imshow("shapes", img)
 cv2.waitKey(0)
